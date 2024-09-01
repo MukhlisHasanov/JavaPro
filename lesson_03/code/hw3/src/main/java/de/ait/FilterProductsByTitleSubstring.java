d330851be8fa4283d40e130bb80b3c007f0db020
MukhlisHasanov <muhlis.gasanov@gmail.com>
2024-09-01 16:13:07 +0400

    hw3 upd

--- lesson_03/code/hw3/src/main/java/de/ait/FilterProductsByTitleSubstring.java
+++ lesson_03/code/hw3/src/main/java/de/ait/FilterProductsByTitleSubstring.java
@@ -6,6 +6,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/** task #4
+ * 4. Допустим у вас дан List<Product> (класс Product реализован на занятии),
+ * необходимо написать метод, который вернет Map<Long,Product> (ключ id, значение product)
+ * содержащий все продукты в название которых есть заданная подстрока.
+ */
+
 public class FilterProductsByTitleSubstring {
     public Map<Long, Product> filterProductsByTitleSubstring(List<Product> products, String substring) {
         return products.stream()
--- lesson_03/code/hw3/src/main/java/de/ait/FindMaxIdInList.java
+++ lesson_03/code/hw3/src/main/java/de/ait/FindMaxIdInList.java
@@ -4,6 +4,11 @@ import app.entity.Product;
 
 import java.util.List;
 
+/** task #3
+ * 3. Допустим у вас дан List<Product> (класс Product реализован на занятии),
+ * необходимо написать метод, который вернет наибольший id в списке.
+ */
+
 public class FindMaxIdInList {
     public Long findMaxId(List<Product> products) {
         return products.stream()
